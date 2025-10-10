66362d56eeab7cd558e37dd6d2ffa3fda6f6bb5b
Melanine12 <159579230+Melanine12@users.noreply.github.com>
2025-10-10 18:16:54 +0100

    Fix: Correction des références de clés étrangères dans le modèle Gratification - utilise tbl_utilisateurs au lieu de tbl_employes

--- server/models/Gratification.js
+++ server/models/Gratification.js
@@ -13,7 +13,7 @@ const Gratification = sequelize.define('Gratification', {
     allowNull: false,
     comment: 'ID de l\'employé concerné',
     references: {
-      model: 'tbl_employes',
+      model: 'tbl_utilisateurs',
       key: 'id'
     }
   },
@@ -60,7 +60,7 @@ const Gratification = sequelize.define('Gratification', {
     allowNull: true,
     comment: 'ID de l\'employé qui a accordé la gratification',
     references: {
-      model: 'tbl_employes',
+      model: 'tbl_utilisateurs',
       key: 'id'
     }
   }
